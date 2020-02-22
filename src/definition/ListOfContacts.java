5ec66f969774625f9e160e42cf08f1e13cfb331b
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-23 00:40:46 +0530

    Created addContact Method

--- src/definition/ListOfContacts.java
+++ src/definition/ListOfContacts.java
@@ -22,17 +22,28 @@ public class ListOfContacts<E> implements ContactsADT<E> {
     private void addAfter (Node <E> node , E data){
         node.next = new Node<>(data , node.next);
         size++;
-
     }
 
-
-
-
-
+    public int getSize() {
+        return size;
+    }
 
     @Override
-    public boolean addContact(E item) {
-        return false;
+    public boolean addContact(int index , E data) {
+        if(index<0 || index >size){
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+
+        }
+        else if(size == 0){
+            addFirst(E data);
+
+        }
+        else{
+            Node<E> count = getNode(index - 1);
+            addAfter(count,data);
+
+        }
+        //return false;
     }
 
     @Override
