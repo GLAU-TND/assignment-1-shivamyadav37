c5691be44cc4be80eb035d0cdb6350c1b3bf44aa
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-23 00:03:59 +0530

    Declared Class ListOfContacts.java for Contact List creating methods in the same class

--- src/definition/ListOfContacts.java
+++ src/definition/ListOfContacts.java
@@ -1,4 +1,39 @@
 package definition;
 
-public class ListOfContacts {
+import adt.ContactsADT;
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+
+public class ListOfContacts<E> implements ContactsADT<E> {
+    ArrayList listName = new ArrayList<String>();
+    ArrayList listNameFirstName = new ArrayList<String>();
+    private Node<E> head;
+    private int size= 0;
+    private int counter = 0;
+
+    private void addFirst();
+
+
+
+
+    @Override
+    public boolean addContact(E item) {
+        return false;
+    }
+
+    @Override
+    public void viewContacts() {
+
+    }
+
+    @Override
+    public void searchInContacts(E item) {
+
+    }
+
+    @Override
+    public boolean deleteFromContacts(int index) {
+        return false;
+    }
 }
