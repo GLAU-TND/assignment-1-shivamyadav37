ac1cecb825c9e2d4ff1e4e224a6ce88d89a81d69
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-22 19:57:36 +0530

    Created methods in ADT

--- src/adt/ContactsADT.java
+++ src/adt/ContactsADT.java
@@ -3,9 +3,11 @@ package adt;
 public interface ContactsADT<E> {
     //for Adding contacts
     boolean addContact(E item);
-
+    //for viewing Contacts
     void viewContacts();
+    //for Searching contacts from the Contact List
     void searchInContacts();
+    //
     void deleteFromContacts();
     void printContact();
 
