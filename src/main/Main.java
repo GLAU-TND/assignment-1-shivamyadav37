b2e6f23c503b2184a05abe7029e77b0985a45bc0
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-23 12:03:58 +0530

    Debugging and Error Correction

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,26 +12,26 @@ public class Main {
         String contactFirstName;
         String contactLastName;
         String[] contactNumber = new String[5];
-        String emailOfContact;
+        String emailOfContact = new String[1];
         boolean ctr;
-        while (ctr=true){
+        while (ctr = true) {
             System.out.println("Welcome to Shivam's Contact List App");
-            System.out.println("Press 1 to add a new contact\n" +"Press 2 to view all contacts\n" +
+            System.out.println("Press 1 to add a new contact\n" + "Press 2 to view all contacts\n" +
                     "Press 3 to search for a contact\n" + "Press 4 to delete a contact\n" + "Press 5 to exit program ");
 
             int choice = scanner.nextInt();
             scanner.nextLine();
-            switch (choice){
+            switch (choice) {
                 case 1:
-                    System.out.println("You have chosen to add a new contact: \n" +"Please enter the name of the Person");
+                    System.out.println("You have chosen to add a new contact: \n" + "Please enter the name of the Person");
                     System.out.println("First Name:");
                     contactFirstName = scanner.nextLine();
                     System.out.println("Last Name:");
                     contactLastName = scanner.nextLine();
-                    listOfContactsObj.setName(contactFirstName,contactLastName);
+                    listOfContactsObj.setName(contactFirstName, contactLastName);
                     listOfContactsObj.setFirstNameInList(contactFirstName);
                     System.out.println("Contact Number:");
-                    contactNumber[0]=scanner.nextLine();
+                    contactNumber[0] = scanner.nextLine();
                     for (int i = 1; i <= 4; i++) {
                         System.out.println("Would you like to add another contact number?");
                         char response = scanner.next().charAt(0);
@@ -49,43 +49,45 @@ public class Main {
                         } else {
                             break;
                         }
+                    }
 
-                    System.out.println("Email Address:");
-                    emailOfContact = scanner.next();
-                    Person person = new Person(contactFirstName,contactLastName,contactNumber,emailOfContact);
-                    listOfContactsObj.addContact(person);
-                    break;
-                case 2:
-                    listOfContactsObj.viewContacts();
-                    break;
-                case 3:
-                    System.out.println("You could search for a contact from their first names: ");
-                    String name = scanner.next();
-                    listOfContactsObj.searchInContacts(name);
-                case 4:
-                    //System.out.println("Here are your contacts");
-                    for (int i =0 ; i<listOfContactsObj.getArrayOfFirstName().size();i++){
-                        System.out.println((i+1)+". "+ listOfContactsObj.getArrayOfFirstName().get(i));
+                            System.out.println("Email Address:");
+                            emailOfContact = scanner.next();
+                            Person person = new Person(contactFirstName, contactLastName, contactNumber, emailOfContact);
+                            listOfContactsObj.addContact(person);
+                            break;
+                            case 2:
+                                listOfContactsObj.viewContacts();
+                                break;
+                            case 3:
+                                System.out.println("You could search for a contact from their first names: ");
+                                String name = scanner.next();
+                                listOfContactsObj.searchInContacts(name);
+                            case 4:
+                                //System.out.println("Here are your contacts");
+                                for (int i = 0; i < listOfContactsObj.getArrayOfFirstName().size(); i++) {
+                                    System.out.println((i + 1) + ". " + listOfContactsObj.getArrayOfFirstName().get(i));
+
+                                }
+                                System.out.println("Press the number against the contact to delete it: ");
+                                int number = scanner.nextInt();
+                                String temp = listOfContactsObj.getArrayOfFirstName().get(number - 1).toString();
+                                listOfContactsObj.deleteFromContacts(number);
+                                System.out.println(temp + " has been deleted from your contacts");
+                                break;
+                            case 5:
+                                System.out.println("Exited Succesfully");
+                                ctr = false;
+                                break;
+                            default:
+                                System.out.println("Please Enter a Valid Input");
+                                break;
+
+
+                        }
 
                     }
-                    System.out.println("Press the number against the contact to delete it: ");
-                    int number = scanner.nextInt();
-                    String temp = listOfContactsObj.getArrayOfFirstName().get(number-1).toString();
-                    listOfContactsObj.deleteFromContacts(number);
-                    System.out.println(temp + " has been deleted from your contacts");
-                    break;
-                case 5:
-                    System.out.println("Exited Succesfully");
-                    ctr = false;
-                    break;
-                default:
-                    System.out.println("Please Enter a Valid Input");
-                    break;
-
 
             }
-
         }
 
-    }
-}
