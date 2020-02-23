9ec113b1522892fc01b5e8d1d5342f9c3daefa1a
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-23 11:13:08 +0530

    Started creating choice in Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,35 @@
 package main;
 
+import definition.ListOfContacts;
+import definition.Person;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ListOfContacts listOfContactsObj = new ListOfContacts();
+        String contactFirstName;
+        String contactLastName;
+        String[] contactNumber;
+        String emailOfContact;
+        boolean ctr;
+        while (ctr==true){
+            System.out.println("Welcome to Shivam's Contact List App");
+            System.out.println("Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice){
+                case 1:
+
+            }
+
+        }
 
     }
 }
